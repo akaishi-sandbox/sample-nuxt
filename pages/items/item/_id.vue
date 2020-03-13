9ea9a73626df04b680d23303cfd6cec0510e2d4a
akaishi <galigalikun@gmail.com>
2020-03-13 12:53:33 +0900

    ロジック切り離し

--- pages/items/item/_id.vue
+++ pages/items/item/_id.vue
@@ -25,86 +25,12 @@ export default {
     breadcrumb: breadcrumb
   },
   computed: {
+    item() {
+      return this.$store.state.item.data;
+    }
   },
-  async asyncData ({ params }) {
-    const { data } = await axios.get(`https://5qh6aaw9u4.execute-api.ap-northeast-1.amazonaws.com/Prod/search-items?item_id=${params.id}`);
-    if (data.total == 0) {
-      // throw new Error("todo")
-    }
-    const shops = [];
-    let imageUrls = [];
-    const detailUrls = {};
-    const contents = {};
-    const lowestPrice = {
-      shop: "",
-      price: 99999999
-    };
-    for (const item of data.items) {
-      const shop = item._source.shop;
-      shops.push(shop);
-      detailUrls[shop] = item._source.detail_url;
-      contents[shop] = item._source.description;
-      if (lowestPrice.price > item._source.lowest_price) {
-          lowestPrice.shop = shop;
-          lowestPrice.price = item._source.lowest_price;
-      }
-      if (item._source.image_urls == undefined) {
-        for (let i = 1; i <= item._source.image_num; i++) {
-          imageUrls.push(`https://d3sg9dzr200rhf.cloudfront.net/images/${item._source.shop}/${item._source.item_id}/main${i}.jpg`);
-        }
-      } else {
-        imageUrls = item._source.image_urls;
-      }
-    }
-    const sizeTable = {
-      headers: [],
-      items: []
-    };
-    if(data.items[0]._source.SKUs) {
-      sizeTable.headers.push({
-        text: "サイズ",
-        sortable: false,
-        value: "size"
-      });
-      for (let i = 0; i < data.items[0]._source.SKUs.length; i++) {
-        const sku = data.items[0]._source.SKUs[i];
-        if (i === 0) {
-          for (const part of sku.parts) {
-            sizeTable.headers.push({
-              text: part.part,
-              sortable: false,
-              value: part.part
-            });
-          }
-        }
-        const sizeInfo = {
-          size: sku.size
-        };
-        for (const part of sku.parts) {
-          sizeInfo[part.part] = part.scale;
-        }
-        sizeTable.items.push(sizeInfo);
-      }
-    }
-    return {
-      item: {
-        title: data.items[0]._source.title,
-        brand: data.items[0]._source.brand,
-        category: data.items[0]._source.category,
-        shops: shops,
-        gender: data.items[0]._source.gender,
-        itemId: data.items[0]._source.item_id,
-        clientId: "todo",
-        imageUrl: imageUrls[0],
-        detailUrls: detailUrls,
-        contents: contents,
-        lowestPrice: lowestPrice,
-        isSized: data.items[0]._source.is_sized,
-        sizeTable: sizeTable,
-        skus: data.items[0]._source.SKUs,
-        color: data.items[0]._source.color
-      }
-    };
+  async fetch ({ store, params }) {
+    await store.dispatch("item/get", params.id);
   }
 }
 </script>
--- store/item.js
+++ store/item.js
@@ -9,8 +9,14 @@ export const state = () => ({
   parameter: {
     gender: "WOMEN",
     category: "",
+    brand: "",
+    discount_flag: "",
     subCategory: "",
-    keywords: ""
+    min_price: 0,
+    max_price: 9999999,
+    keywords: "",
+    order: "",
+    offset: 0
   },
   genders: [
     {
@@ -26,6 +32,7 @@ export const state = () => ({
   total: 0,
   categories: [],
   subCategories: [],
+  data: {},
   list: []
 });
 
@@ -84,12 +91,25 @@ export const actions = {
     );
     commit("SET_RESULT", data);
   },
+  async get({ commit }, itemId) {
+    const { data } = await axios.get(`https://5qh6aaw9u4.execute-api.ap-northeast-1.amazonaws.com/Prod/search-items`,
+      {
+        params: {
+          item_id: itemId
+        }
+      }
+    );
+    commit("SET_DATA", data);
+  },
   async search({ commit }, payload) {
     commit("SET_PARAMETER", payload);
     const { data } = await axios.get(
       `https://5qh6aaw9u4.execute-api.ap-northeast-1.amazonaws.com/Prod/search-items`,
       {
-        params: this.state.item.parameter
+        params: {
+          ...this.state.item.parameter,
+          limit: 36
+        }
       }
     );
     commit("SET_RESULT", data);
@@ -147,10 +167,105 @@ export const mutations = {
           state.parameter.keywords = data.subCategory;
         }
       }
-      if (data.keyword != undefined && data.keyword) {
+      if (data.brand != undefined) {
+        state.parameter.brand = data.brand;
+      }
+      if (data.discount_flag != undefined) {
+        state.parameter.discount_flag = data.discount_flag;
+      }
+      if (data.min_price != undefined && data.min_price > 0) {
+        state.parameter.min_price = data.min_price;
+      }
+      if (data.max_price != undefined && 300000 > data.max_price && data.max_price > 0) {
+        state.parameter.max_price = data.max_price;
+      }
+      if (data.keyword != undefined && data.keyword.length > 0) {
         state.parameter.keywords = data.keyword;
       }
+      if (data.order != undefined) {
+        state.parameter.order = data.order;
+      }
+      if (data.offset != undefined) {
+        state.parameter.offset = data.offset;
+      }
     }
+  },
+  SET_DATA(state, data) {
+    if (data.total == 0) {
+      // throw new Error("todo");
+    }
+    const shops = [];
+    let imageUrls = [];
+    const detailUrls = {};
+    const contents = {};
+    const lowestPrice = {
+      shop: "",
+      price: 99999999
+    };
+    for (const item of data.items) {
+      const shop = item._source.shop;
+      shops.push(shop);
+      detailUrls[shop] = item._source.detail_url;
+      contents[shop] = item._source.description;
+      if (lowestPrice.price > item._source.lowest_price) {
+        lowestPrice.shop = shop;
+        lowestPrice.price = item._source.lowest_price;
+      }
+      if (item._source.image_urls == undefined) {
+        for (let i = 1; i <= item._source.image_num; i++) {
+          imageUrls.push(`https://d3sg9dzr200rhf.cloudfront.net/images/${item._source.shop}/${item._source.item_id}/main${i}.jpg`);
+        }
+      } else {
+        imageUrls = item._source.image_urls;
+      }
+    }
+    const sizeTable = {
+      headers: [],
+      items: []
+    };
+    if (data.items[0]._source.SKUs) {
+      sizeTable.headers.push({
+        text: "サイズ",
+        sortable: false,
+        value: "size"
+      });
+      for (let i = 0; i < data.items[0]._source.SKUs.length; i++) {
+        const sku = data.items[0]._source.SKUs[i];
+        if (i === 0) {
+          for (const part of sku.parts) {
+            sizeTable.headers.push({
+              text: part.part,
+              sortable: false,
+              value: part.part
+            });
+          }
+        }
+        const sizeInfo = {
+          size: sku.size
+        };
+        for (const part of sku.parts) {
+          sizeInfo[part.part] = part.scale;
+        }
+        sizeTable.items.push(sizeInfo);
+      }
+    }
+    state.data = {
+      title: data.items[0]._source.title,
+      brand: data.items[0]._source.brand,
+      category: data.items[0]._source.category,
+      shops: shops,
+      gender: data.items[0]._source.gender,
+      itemId: data.items[0]._source.item_id,
+      clientId: "todo",
+      imageUrl: imageUrls[0],
+      detailUrls: detailUrls,
+      contents: contents,
+      lowestPrice: lowestPrice,
+      isSized: data.items[0]._source.is_sized,
+      sizeTable: sizeTable,
+      skus: data.items[0]._source.SKUs,
+      color: data.items[0]._source.color
+    };
   },
   SET_RESULT(state, data) {
     state.total = data.total;
